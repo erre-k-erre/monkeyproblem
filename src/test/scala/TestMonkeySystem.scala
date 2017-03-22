1a38b9a0d134cbaa6617287c2fad01dfae77514d
Alvaro Santuy Elena <Alvaro Santuy Elena>
2017-03-22 02:05:54 +0100

    Refactor tests.

--- src/test/scala/TestMonkeySystem.scala
+++ src/test/scala/TestMonkeySystem.scala
@@ -5,14 +5,15 @@ import akka.testkit.TestActorRef
 import monkey.actor.{MonkeyMonitor, MonkeyParent}
 
 /**
-  * Created by asantuy on 16/03/2017.
+  * This class wraps a given Actor System and sets it for testing. It allows to reset the system so it could be used in
+  * different tests.
   */
-object TestMonkeySystem {
+class TestMonkeySystem(val system: ActorSystem, val ft: ActorRef) {
 
     var monitor: TestActorRef[MonkeyMonitor] = _
     var parent: ActorRef = _
 
-    def reset(system: ActorSystem, ft: ActorRef): Unit = {
+    def reset(): Unit = {
         if (monitor != null) system stop monitor
         if (parent != null) system stop parent
         monitor = TestActorRef.create(system, MonkeyMonitor.props(ft))
--- src/test/scala/TestSuite.scala
+++ src/test/scala/TestSuite.scala
@@ -13,14 +13,16 @@ import scala.concurrent.duration._
 class TestSuite() extends TestKit(ActorSystem()) with ImplicitSender
     with WordSpecLike with Matchers with BeforeAndAfterAll {
 
+    val testSystem = new TestMonkeySystem(system, this.testActor)
+
     override def afterAll {
         TestKit.shutdownActorSystem(system)
     }
 
     "A lonely monkey" must {
         "be dequeued in around 5 seconds" in {
-            TestMonkeySystem.reset(system, this.testActor)
-            TestMonkeySystem.parent ! EastToWest
+            testSystem.reset()
+            testSystem.parent ! EastToWest
             expectMsg(EastToWest)
             expectNoMsg(1.second)
             expectMsg(0.1.seconds, OnTheRope)
@@ -32,9 +34,9 @@ class TestSuite() extends TestKit(ActorSystem()) with ImplicitSender
 
     "Two monkeys crossing and arriving at the same time" must {
         "be dequeued" in {
-            TestMonkeySystem.reset(system, this.testActor)
-            TestMonkeySystem.parent ! EastToWest
-            TestMonkeySystem.parent ! WestToEast
+            testSystem.reset()
+            testSystem.parent ! EastToWest
+            testSystem.parent ! WestToEast
             expectMsgAnyOf(EastToWest, WestToEast)
             expectMsgAnyOf(EastToWest, WestToEast)
             expectMsg(1.1.seconds, OnTheRope)
@@ -44,18 +46,18 @@ class TestSuite() extends TestKit(ActorSystem()) with ImplicitSender
             expectMsg(0.1.seconds, OnTheRope)
             expectNoMsg(4.second)
             expectMsg(0.1.seconds, Done)
-            assert(TestMonkeySystem.monitor.underlyingActor.passing.isEmpty, "Monkey has not passed.")
-            assert(TestMonkeySystem.monitor.underlyingActor.fromEast.isEmpty, "Monkey from East is still enqueued.")
-            assert(TestMonkeySystem.monitor.underlyingActor.fromWest.isEmpty, "Monkey from West is still enqueued.")
+            assert(testSystem.monitor.underlyingActor.passing.isEmpty, "Monkey has not passed.")
+            assert(testSystem.monitor.underlyingActor.fromEast.isEmpty, "Monkey from East is still enqueued.")
+            assert(testSystem.monitor.underlyingActor.fromWest.isEmpty, "Monkey from West is still enqueued.")
         }
     }
 
     "Two monkeys crossing and arriving at different time" must {
         "be dequeued" in {
-            TestMonkeySystem.reset(system, this.testActor)
-            TestMonkeySystem.parent ! EastToWest
+            testSystem.reset()
+            testSystem.parent ! EastToWest
             Thread.sleep(200)
-            TestMonkeySystem.parent ! WestToEast
+            testSystem.parent ! WestToEast
             expectMsg(EastToWest)
             expectMsg(WestToEast)
             expectMsg(1.seconds, OnTheRope)
@@ -65,17 +67,17 @@ class TestSuite() extends TestKit(ActorSystem()) with ImplicitSender
             expectMsg(0.1.seconds, OnTheRope)
             expectNoMsg(4.second)
             expectMsg(0.1.seconds, Done)
-            assert(TestMonkeySystem.monitor.underlyingActor.passing.isEmpty, "Monkey has not passed.")
-            assert(TestMonkeySystem.monitor.underlyingActor.fromEast.isEmpty, "Monkey from East is still enqueued.")
-            assert(TestMonkeySystem.monitor.underlyingActor.fromWest.isEmpty, "Monkey from West is still enqueued.")
+            assert(testSystem.monitor.underlyingActor.passing.isEmpty, "Monkey has not passed.")
+            assert(testSystem.monitor.underlyingActor.fromEast.isEmpty, "Monkey from East is still enqueued.")
+            assert(testSystem.monitor.underlyingActor.fromWest.isEmpty, "Monkey from West is still enqueued.")
         }
     }
 
     "Two monkeys going in the same direction and arriving at the same time" must {
         "be dequeued" in {
-            TestMonkeySystem.reset(system, this.testActor)
-            TestMonkeySystem.parent ! EastToWest
-            TestMonkeySystem.parent ! EastToWest
+            testSystem.reset()
+            testSystem.parent ! EastToWest
+            testSystem.parent ! EastToWest
             expectMsg(EastToWest)
             expectMsg(EastToWest)
             expectMsg(1.1.seconds, OnTheRope)
@@ -84,37 +86,37 @@ class TestSuite() extends TestKit(ActorSystem()) with ImplicitSender
             expectMsg(3.1.seconds, Done)
             expectNoMsg(1.second)
             expectMsg(0.1.seconds, Done)
-            assert(TestMonkeySystem.monitor.underlyingActor.fromEast.isEmpty, "Monkey is still enqueued.")
-            assert(TestMonkeySystem.monitor.underlyingActor.passing.isEmpty, "Monkey has not passed.")
+            assert(testSystem.monitor.underlyingActor.fromEast.isEmpty, "Monkey is still enqueued.")
+            assert(testSystem.monitor.underlyingActor.passing.isEmpty, "Monkey has not passed.")
         }
     }
 
     "Three monkeys going in different directions" must {
         "be dequeued" in {
-            TestMonkeySystem.reset(system, this.testActor)
-            TestMonkeySystem.parent ! EastToWest
+            testSystem.reset()
+            testSystem.parent ! EastToWest
             Thread.sleep(200)
-            TestMonkeySystem.parent ! WestToEast
+            testSystem.parent ! WestToEast
             Thread.sleep(200)
-            TestMonkeySystem.parent ! EastToWest
+            testSystem.parent ! EastToWest
             expectMsg(EastToWest)
             expectMsg(WestToEast)
             expectMsg(EastToWest)
             expectMsg(OnTheRope)
-            assert(TestMonkeySystem.monitor.underlyingActor.fromWest.size == 1, "Monkey from West did not wait.")
-            assert(TestMonkeySystem.monitor.underlyingActor.fromEast.size == 1, "Monkey from East did not wait.")
+            assert(testSystem.monitor.underlyingActor.fromWest.size == 1, "Monkey from West did not wait.")
+            assert(testSystem.monitor.underlyingActor.fromEast.size == 1, "Monkey from East did not wait.")
             expectMsg(4.1.seconds, Done)
             expectMsg(OnTheRope)
-            assert(TestMonkeySystem.monitor.underlyingActor.fromWest.isEmpty, "Monkey from West is still enqueued.")
-            assert(TestMonkeySystem.monitor.underlyingActor.fromEast.size == 1, "Monkey from East did not wait.")
+            assert(testSystem.monitor.underlyingActor.fromWest.isEmpty, "Monkey from West is still enqueued.")
+            assert(testSystem.monitor.underlyingActor.fromEast.size == 1, "Monkey from East did not wait.")
             expectMsg(4.1.seconds, Done)
             expectMsg(OnTheRope)
-            assert(TestMonkeySystem.monitor.underlyingActor.fromWest.isEmpty, "Monkey from West is still enqueued.")
-            assert(TestMonkeySystem.monitor.underlyingActor.fromEast.isEmpty, "Monkey from East is still enqueued.")
+            assert(testSystem.monitor.underlyingActor.fromWest.isEmpty, "Monkey from West is still enqueued.")
+            assert(testSystem.monitor.underlyingActor.fromEast.isEmpty, "Monkey from East is still enqueued.")
             expectMsg(4.1.seconds, Done)
-            assert(TestMonkeySystem.monitor.underlyingActor.passing.isEmpty, "Monkey has not passed.")
-            assert(TestMonkeySystem.monitor.underlyingActor.fromEast.isEmpty, "Monkey from East is still enqueued.")
-            assert(TestMonkeySystem.monitor.underlyingActor.fromWest.isEmpty, "Monkey from West is still enqueued.")
+            assert(testSystem.monitor.underlyingActor.passing.isEmpty, "Monkey has not passed.")
+            assert(testSystem.monitor.underlyingActor.fromEast.isEmpty, "Monkey from East is still enqueued.")
+            assert(testSystem.monitor.underlyingActor.fromWest.isEmpty, "Monkey from West is still enqueued.")
 
         }
     }
